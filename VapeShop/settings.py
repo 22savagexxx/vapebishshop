bcf964e7fd7333f1a5f1786745a4b24cf9ecd36f
22savagexxx <staalaibekov2@mail.ru>
2022-09-09 17:16:01 +0600

    33

--- VapeShop/settings.py
+++ VapeShop/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,5 +121,5 @@ STATICFILES_DIRS = [
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
